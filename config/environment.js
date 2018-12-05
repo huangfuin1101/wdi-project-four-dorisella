86187480547797f582a6c34965422935620885ff
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-12-05 14:38:50 +0000

    setup environment file

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,6 @@
 const port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'dev';
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/'bag${env}`;
+const secret = process.env.SECRET || 'buy';
 
-module.exports = { port };
+module.exports = { port, env, dbURI, secret};
